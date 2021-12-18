06ea898195b82c4f5f4f3366060e9859f0d2293d
Lada496 <67321065+Lada496@users.noreply.github.com>
2021-12-17 23:45:28 -0800

    brought back next.config

--- next.config.js
+++ next.config.js
@@ -1,30 +1,12 @@
-const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
-module.exports = (phase) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return {
-      env: {
-        mongodb_username: process.env.mongodb_username,
-        mongodb_password: process.env.mongodb_password,
-        mongodb_cluster: process.env.mongodb_cluster,
-        mongodb_database: process.env.mongodb_database,
-        stripe_key: process.env.stripe_key,
-        NEXTAUTH_URL: "http://localhost:3000/api/auth",
-      },
-      images: {
-        domains: ["fakestoreapi.com"],
-      },
-    };
-  }
-  return {
-    env: {
-      mongodb_username: process.env.mongodb_username,
-      mongodb_password: process.env.mongodb_password,
-      mongodb_cluster: process.env.mongodb_cluster,
-      mongodb_database: process.env.mongodb_database,
-      stripe_key: process.env.stripe_key,
-    },
-    images: {
-      domains: ["fakestoreapi.com"],
-    },
-  };
-};
+module.exports = () => ({
+  env: {
+    mongodb_username: process.env.mongodb_username,
+    mongodb_password: process.env.mongodb_password,
+    mongodb_cluster: process.env.mongodb_cluster,
+    mongodb_database: process.env.mongodb_database,
+    stripe_key: process.env.stripe_key,
+  },
+  images: {
+    domains: ["fakestoreapi.com"],
+  },
+});
