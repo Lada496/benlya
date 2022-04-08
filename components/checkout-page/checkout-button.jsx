cef444872ef0156d603a19fc6565c2c157cebd39
Lada496 <67321065+Lada496@users.noreply.github.com>
2022-04-08 14:07:50 -0700

    modified checkout operations

--- components/checkout-page/checkout-button.jsx
+++ components/checkout-page/checkout-button.jsx
@@ -1,14 +1,22 @@
-import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import StripeCheckout from "react-stripe-checkout";
 import { resetCart } from "../../redux/cart/cart.actions";
+import { removeItemfromWishlistAction } from "../../redux/wishlist/whishlist.actions";
 
 const CheckoutButton = ({ price }) => {
   const dispatch = useDispatch();
+  const cartItems = useSelector((state) => state.cart.cartItems);
   const priceForStripe = price * 100;
   const publishableKey = process.env.stripe_key;
   const onToken = (token) => {
-    alert("Payment Successful");
     dispatch(resetCart());
+
+    // delete purchased items from wishlist
+    for (const item of cartItems) {
+      dispatch(removeItemfromWishlistAction(item));
+    }
+    alert("Payment Successful");
   };
   return (
     <StripeCheckout
