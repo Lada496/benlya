b932e0b08c8c04afe3053d875d8237413e6af9c5
Lada496 <67321065+Lada496@users.noreply.github.com>
2023-05-27 17:49:56 -0700

    added redirect at auth page to aviod logged-in users to visit

--- app/auth/page.js
+++ app/auth/page.js
@@ -1,10 +1,17 @@
 "use client";
 import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
 import Login from "../../components/auth-page/login";
 import SignUp from "../../components/auth-page/signup";
 
 const AuthPage = () => {
   const [isLogin, setIsLogin] = useState(true);
+  const { data: session } = useSession();
+
+  if (session) {
+    redirect("/");
+  }
 
   return (
     <>
