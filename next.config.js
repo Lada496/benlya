dbe595b3dadcb0a90dae54c23970100237fe5118
Lada496 <67321065+Lada496@users.noreply.github.com>
2023-05-28 14:23:59 -0700

    get back topLevelAwait: true

--- next.config.js
+++ next.config.js
@@ -15,4 +15,8 @@ module.exports = () => ({
     domains: ["fakestoreapi.com"],
   },
   experimental: { appDir: true },
+  webpack(config) {
+    config.experiments = { ...config.experiments, topLevelAwait: true };
+    return config;
+  },
 });
