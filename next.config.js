4d2f9b09fbcd15086e962bf02f113a92de288f5b
Lada496 <67321065+Lada496@users.noreply.github.com>
2023-05-28 11:37:13 -0700

    fixed button, tried new settings

--- next.config.js
+++ next.config.js
@@ -15,8 +15,4 @@ module.exports = () => ({
     domains: ["fakestoreapi.com"],
   },
   experimental: { appDir: true },
-  webpack(config) {
-    config.experiments = { ...config.experiments, topLevelAwait: true };
-    return config;
-  },
 });
